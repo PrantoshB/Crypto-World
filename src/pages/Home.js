82cd353f0efe3a7451bd661a07c091e27f7986dc
PrantoshB <pbiswas1007@gmail.com>
2023-02-08 00:32:30 +0530

    Refactor Homepage code

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 import Coin from '../components/Coin';
 import { fetchCoinsAction } from '../redux/coins/coins';
 import Global from '../components/Global';
+import Details from './Details';
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -18,13 +20,14 @@ const Home = () => {
       <div className="cointop"><p>Stats by cryptocurrencies</p></div>
       <div className="coin-container">
         {coins.map((coin) => (
-          <Coin
-            id={coin.id}
-            key={coin.id}
-            name={coin.name}
-            image={coin.image}
-            price={coin.current_price}
-          />
+          <Link to={`/details/${coin.id}`} key={coin.id} element={<Details />}>
+            <Coin
+              id={coin.id}
+              name={coin.name}
+              image={coin.image}
+              price={coin.current_price}
+            />
+          </Link>
         ))}
       </div>
     </>
